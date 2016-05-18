c131b14e1db91cf705d73b016862bcf56c294407
Domenico Fabio Marino <dfm114@ic.ac.uk>
2016-05-18 10:14:37 +0100

    Backend: Yahoo_Rest: reformat
    Signed-off-by: Domenico Fabio Marino <dfm114@ic.ac.uk>

--- yahoo_rest.rb
+++ yahoo_rest.rb
@@ -1,28 +1,29 @@
+#!/usr/bin/ruby
+
 require 'rest-client'
 def doRequest(params)
   RestClient.get('http://download.finance.yahoo.com/d/quotes.csv',
-      { :params => params }
-    ) do |response|
-     return response
-    end
+                 params: params) do |response|
+    return response
+  end
 end
 
 def requestName(symbol)
-  response = doRequest({:s => symbol, :f => 'n'})
-  return response
+  response = doRequest(s: symbol, f: 'n')
+  response
 end
 
 def requestBid(symbol)
-  response = doRequest({:s => symbol, :f => 'b'})
-  return response
+  response = doRequest(s: symbol, f: 'b')
+  response
 end
 
 def requestAsk(symbol)
-  response = doRequest({:s => symbol, :f => 'a'})
-  return response
+  response = doRequest(s: symbol, f: 'a')
+  response
 end
 
 def checkExistance(symbol)
-  response = doRequest({:s => symbol, :f => 'n'})
-  return !(response.eql? "N/A\n")
+  response = doRequest(s: symbol, f: 'n')
+  !(response.eql? "N/A\n")
 end
