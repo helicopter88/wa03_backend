d15d68f03634d2da7f8bdb40ffc0d00f2cea6998
Ubuntu <Domenico Fabio Marino>
2016-05-23 12:06:17 +0000

    Backend: WebSocket: add comments, add missing APIs
    Signed-off-by: Ubuntu <Domenico Fabio Marino>

--- web_socket.rb
+++ web_socket.rb
@@ -25,31 +25,54 @@ EM.run do
       tokens = msg.split
       # The first one is always going to be the action we want to execute
       case tokens[0]
+      # ask_price symbol
       when 'ask_price'
         ws.send yr.request_ask(tokens[1])
+      # bid_price symbol
       when 'bid_price'
         ws.send yr.request_bid(tokens[1])
+      # exists symbol
       when 'exists'
         ws.send yr.check_existance(tokens[1])
-      
+      #
+      # END OF YAHOO TOKENS
+      #
+
+      # TODO: potentially useless
+      # check_instr symbol name
       when 'check_instr'
 	ws.send db.check_instr tokens[1], tokens[2]      	
+      # login user password
       when 'login'
         ws.send db.check_user tokens[1], tokens[2]
+      # insert_user username password capital
+      # capital is casted to float
       when 'insert_user'
-        ws.send db.insert_user tokens[1], tokens[2], tokens[3]
-
+        ws.send db.insert_user tokens[1], tokens[2], tokens[3].to_f
+      # TODO: this table is potentially useless
+      # insert_instr symbol name
       when 'insert_instr'
 	ws.send db.insert_instrument tokens[1], tokens[2]
-
+      # insert_trans user symbol price amount "true for buy | false for sell"
+      # price casted to float, amount to int
       when 'insert_trans'
-	ws.send db.insert_trans tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]
-
+	ws.send db.insert_trans tokens[1], tokens[2], tokens[3].to_f, tokens[4].to_i, tokens[5]
+      # get_capital user
       when 'get_capital'
-	ws.send db.get_user_capital tokens[1], tokens[2]
-
+	ws.send db.get_user_capital tokens[1]
+      # get_buy_trans user
       when 'get_buy_trans'
-	ws.send db.get_buy_trans tokens[1]      
+	ws.send db.get_buy_trans tokens[1]
+      # update_user_cap user newcapital
+      # newcapital casted to float
+      when 'update_user_cap'
+	ws.send db.update_user_capital tokens[1], tokens[2].to_f
+      # get_total_profit user
+      when 'get_total_profit'
+	ws.send db.get_total_profit tokens[1] 
+      #
+      # END OF DB TOKENS
+      #
       else
         ws.send "Invalid Message: #{msg}"
       end
