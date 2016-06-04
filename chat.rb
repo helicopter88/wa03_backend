da49ba965b7c0fad3112265072c0f1f2e2550b92
Domenico Fabio Marino <dfm114@ic.ac.uk>
2016-06-04 15:24:22 +0000

    Backend: Chat: make it working
    Signed-off-by: Domenico Fabio Marino <dfm114@ic.ac.uk>

--- chat.rb
+++ chat.rb
@@ -1,39 +1,40 @@
 require 'em-websocket'
+
 class Chat
   def initialize
     @channels = Hash.new
   end
-  def new(ws, cname)
+  
+  def method_missing(m, *args, &block)
+    puts "Invalid method called"
+  end
+
+  def new_chan(ws, cname, user)
     @channels[cname] = EM::Channel.new
-    subscribe(ws, cname)
+    subscribe(ws, cname, user)
   end
 
-  def subscribe(ws, cname)
-    sid = @channels[cname].subscribe {|m| ws.send(m) }
-    @channels[cname].push ("#{sid} joined the chat")
+  def subscribe(ws, cname, user)
+    @channels[cname].subscribe {|m| ws.send(m) }
+    @channels[cname].push ("#{user} joined the chat")
   end
 
-  def send_msg(name, user, msg)
-    @channels[name].push {"#{user}: #{msg}"}
+  def send_msg(ws, cname, user, *msg)
+    str = ''
+    msg.map {|w| str += "#{w} "}
+    @channels[cname].push "#{user}: #{str}"
   end
+end
+
+@chat = Chat.new
 
 EventMachine.run {
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8081) do |ws|
     ws.onopen { |handshake| puts "#{handshake} connected"  }
     ws.onmessage do |msg|
       tokens = msg.split
-      case tokens.shift
-        when "new"
-         new(ws, tokens.shift)
-        when "subscribe"
-          subscribe(ws, tokens.shift)
-        when "send_msg"
-          send_msg(tokens.shift, tokens.shift, tokens.shift)
-        else
-          ws.send "Invalid message"
-      end
-      end
+      @chat.send(tokens.shift, ws, *tokens) 
     end
+  end
+}
 
-  }
-end
